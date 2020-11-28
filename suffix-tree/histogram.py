e3d313bcc300dcdfc934a044c7fa691110824046
Gabriel Evaristo <you@example.com>
2020-11-28 20:50:23 -0300

    ....

--- suffix-tree/histogram.py
+++ suffix-tree/histogram.py
@@ -1,8 +1,5 @@
 #!./pypy3.7-v7.3.2-linux64/bin/pypy3
 import argparse
-import matplotlib
-import matplotlib.pyplot as plt
-import collections
 import simplejson as json
 
 parser = argparse.ArgumentParser(
@@ -14,6 +11,7 @@ parser.add_argument("-out", "--output", dest="output")
 parser.add_argument("-v", "--verbose", action="store_true")
 args = parser.parse_args()
 
+program_name = args.stat_trace.split('.')[0]
 #####
 # STAT
 # assembler_instruction, opcode_type, instruction_addr,
@@ -37,8 +35,7 @@ def write_next_fields(instruction, instruction_field, instruction_field_value, i
 
     return instruction
 
-memory_instructions_in_block = []
-number_of_memory_instructions_in_block = 0
+
 stat_trace_aux = []
 stat_trace = []
 with open(args.stat_trace, "r") as stat:
@@ -51,9 +48,6 @@ with open(args.stat_trace, "r") as stat:
         if line.startswith("@"):
             stat_trace.append(stat_trace_aux)
             stat_trace_aux = []
-
-            memory_instructions_in_block.append(number_of_memory_instructions_in_block)
-            number_of_memory_instructions_in_block = 0
 
         else:
             line_iter = iter(line.split())
@@ -92,7 +86,6 @@ with open(args.stat_trace, "r") as stat:
                         (instruction_field == "is_read_flag" or instruction_field == "is_write_flag")
                         and instruction_field_value == "1"
                     ):
-                        number_of_memory_instructions_in_block += 1
                         instruction.update(
                             {instruction_field: instruction_field_value}
                         )
@@ -102,7 +95,6 @@ with open(args.stat_trace, "r") as stat:
                         instruction_field == "is_read2_flag"
                         and instruction_field_value == "1"
                     ):
-                        number_of_memory_instructions_in_block += 2
                         instruction.update(
                             {instruction_field: instruction_field_value}
                         )
@@ -121,39 +113,47 @@ with open(args.stat_trace, "r") as stat:
         line = stat.readline()
 
     stat_trace.append(stat_trace_aux)
-    memory_instructions_in_block.append(number_of_memory_instructions_in_block)
+
+with open("stat_memory_instructions_" + program_name + ".json", "w+") as stat_info_json:
+    json.dump(stat_trace, stat_info_json)
 
 
 def updateStrideInAccess(info, address, access):
-    stride = int(address) - info["last_address_in_" + access]
+    if info["last_address_in_" + access]:
+        stride = int(address) - info["last_address_in_" + access]
+    else:
+        stride = int(address)
+
     if stride in info[access + "_strides"].keys():
         info[access + "_strides"].update(
             {stride: info[access + "_strides"][stride] + 1}
         )
     else:
         info[access + "_strides"].update({stride: 1})
+
     info.update({"last_address_in_" + access: int(address)})
     return info
 
-
+i = 0
 memory_instructions_info = {}
 with open(args.mem_trace, "r") as mem:
     line = mem.readline()
-    i = 1
     while line:
         if line.startswith("#"):
             line = mem.readline()
             continue
-        
-        if i >= 3:
+        i += 1
+        if i == 15:
             break
-
         op, size, address, block = line.split()
-        
-        for j in range(memory_instructions_in_block[int(block)]):
-            op, size, address, block = line.split()
+        blk = int(block)
+        for j in range(len(stat_trace[blk])):
+            print(blk, j)
+            print(stat_trace[blk][j])
+            print(line)
 
-            instruction = stat_trace[int(block)][j]["instruction_addr"]
+            op, size, address, block = line.split()
+            instruction = stat_trace[blk][j]["instruction_addr"]
             info = {
                 "access1_strides": {},
                 "last_address_in_access1": None,
@@ -164,58 +164,37 @@ with open(args.mem_trace, "r") as mem:
                 "count": 0,
             }
 
-            if stat_trace[int(block)][j]["is_read_flag"] == "1":
+            if stat_trace[blk][j]["is_read_flag"] == "1":
                 if instruction in memory_instructions_info.keys():
-                    info = memory_instructions_info[instruction]
-                    info = updateStrideInAccess(info, address, "access1")
-
+                    info = updateStrideInAccess(memory_instructions_info[instruction], address, "access1")
                 else:
-                    info["access1_strides"].update({int(address): 1})
-                    info.update({"last_address_in_access1": int(address)})
+                    info = updateStrideInAccess(info, address, "access1")
                 
-            elif stat_trace[int(block)][j]["is_read2_flag"] == "1":
+            elif stat_trace[blk][j]["is_read2_flag"] == "1":
                 if instruction in memory_instructions_info.keys():
-                    info = memory_instructions_info[instruction]
+                    info = updateStrideInAccess(memory_instructions_info[instruction], address, "access1")
+
+                    line = mem.readline()
+                    op, size, address, block = line.split()
+
+                    info = updateStrideInAccess(info, address, "access2")
+                else:
                     info = updateStrideInAccess(info, address, "access1")
 
-                    op, size, address, block = mem.readline().split()
-                    i = i + 1
-                    if i >= 3:
-                        break
+                    line = mem.readline()
+                    op, size, address, block = line.split()
+
                     info = updateStrideInAccess(info, address, "access2")
 
-                else:
-                    info["access1_strides"].update({int(address): 1})
-                    info.update({"last_address_in_access1": int(address)})
-
-                    op, size, address, block = mem.readline().split()
-                    i = i + 1
-                    if i >= 3:
-                        break
-                    info["access2_strides"].update({int(address): 1})
-                    info.update({"last_address_in_access2": int(address)})
-
-            if stat_trace[int(block)][j]["is_write_flag"] == "1":
+            if stat_trace[blk][j]["is_write_flag"] == "1":
                 if instruction in memory_instructions_info.keys():
-                    op, size, address, block = mem.readline().split()
-                    i = i + 1
-                    if i >= 3:
-                        break
-                    info = updateStrideInAccess(info, address, "access3")
-
+                    info = updateStrideInAccess(memory_instructions_info[instruction], address, "access3")
                 else:
-                    op, size, address, block = mem.readline().split()
-                    i = i + 1
-                    if i >= 3:
-                        break
-                    info["access3_strides"].update({int(address): 1})
-                    info.update({"last_address_in_access3": int(address)})
+                    info = updateStrideInAccess(info, address, "access3")
 
             info.update({"count": info["count"] + 1})
             memory_instructions_info.update({instruction: info})
             line = mem.readline()
-            i = i + 1
 
-program_name = args.stat_trace.split('.')[0]
 with open("strides_" + program_name + ".json", "w+") as memory_info_json:
     json.dump(memory_instructions_info, memory_info_json)
