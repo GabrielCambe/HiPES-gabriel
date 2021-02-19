29223b92194554bdd806ebae72b8dcbad3ac7e0c
Gabriel Evaristo <gabriel@rd2.ventures>
2021-02-19 09:54:10 -0300

    ...

--- OrCS/stride_analysis_v2.hpp
+++ OrCS/stride_analysis_v2.hpp
@@ -47,13 +47,13 @@ class StatusMachine {
                 case UNINITIALIZED:
                 case NON_LINEAR:
                     if (first_address){
-                        printf("1ST ACCESS!!");
+                        // printf("1ST ACCESS!!");
                         first_address = false;
                         first_stride = true;
                         return LEARN;
                     }
                     if (first_stride){
-                        printf("\t\t\t1ST STRIDE!!");
+                        // printf("\t1ST STRIDE!!");
                         first_stride = false;
                         last_stride = stride;
                         return LEARN;
@@ -61,9 +61,11 @@ class StatusMachine {
                     if (last_stride == stride) {
                         eqCount++;
                         if (eqCount == 4){
-                            printf("\t\t\t\t\t\tEQUAL ACCESS!!");
+                            // printf("\t\tEQUAL ACCESS!!");
                             current_status = STEADY;
                             eqCount = 0;
+                            return current_status;
+
                         } else {
                             return LEARN;
                         }
@@ -73,6 +75,7 @@ class StatusMachine {
                 case STEADY:
                     if (last_stride != stride) {
                         current_status = NON_LINEAR; 
+                        return current_status;
                     }
                     break;
             }
