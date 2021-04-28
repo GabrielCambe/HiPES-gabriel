fe96a12e0e2738c6fbc7484c1e20d64b9cc3db69
Gabriel Evaristo <gabriel@rd2.ventures>
2021-04-28 13:18:10 -0300

    ...

--- OrCS/simulator.cpp
+++ OrCS/simulator.cpp
@@ -322,7 +322,7 @@ int main(int argc, char **argv) {
                 if(memory_instructions_info[i][j].info.instruction.status != UNINITIALIZED){
                     memory_instructions_counted += memory_instructions_info[i][j].info.instruction.count;
 
-                    if (memory_instructions_info[i][j].info.instruction.integrally_steady){
+                    if (memory_instructions_info[i][j].info.instruction.integrally_steady && memory_instructions_info[i][j].info.instruction.count > 1){
                         accesses_in_integrally_steady_instructions += (memory_instructions_info[i][j].info.read.count + memory_instructions_info[i][j].info.read2.count + memory_instructions_info[i][j].info.write.count);
 
                         if(memory_instructions_info[i][j].info.read.count > 0) {
